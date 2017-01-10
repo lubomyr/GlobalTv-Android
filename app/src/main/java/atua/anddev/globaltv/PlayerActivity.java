a7602bf0ced8227ac831bfa6722faea9aae575eb
lubomyr <lubomyr31@gmail.com>
2017-01-10 16:46:04 +0200

    Implemented built-in player

--- app/src/main/java/atua/anddev/globaltv/PlayerActivity.java
+++ app/src/main/java/atua/anddev/globaltv/PlayerActivity.java
@@ -1,24 +1,71 @@
 package atua.anddev.globaltv;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
-public class PlayerActivity extends Activity {
-    VideoView videoView;
-    String selectedUrl;
+public class PlayerActivity extends Activity implements MediaPlayer.OnCompletionListener,
+        MediaPlayer.OnInfoListener {
+    private MediaController mediaControls;
+    private VideoView mVideoView;
+    private Uri uri;
+    private Integer position;
 
-    public void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Set sub.xml as user interface layout
         setContentView(R.layout.player);
-        videoView = (VideoView) findViewById(R.id.videoView1);
-        Uri uri = Uri.parse(selectedUrl);
-        videoView.setVideoURI(uri);
-        videoView.requestFocus();
-        videoView.start();
 
+        if (mediaControls == null)
+            mediaControls = new MediaController(this);
+
+        getData();
+        startPlayer();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("Position", mVideoView.getCurrentPosition());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        int position = savedInstanceState.getInt("Position");
+        mVideoView.seekTo(position);
+    }
+
+    @Override
+    public boolean onInfo(MediaPlayer p1, int p2, int p3) {
+        return false;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer p1) {
+        if (!p1.isPlaying()) {
+            startPlayer();
+        }
+    }
+
+    private void getData() {
+        Intent intent = getIntent();
+        position = intent.getIntExtra("Position", 0);
+        uri = intent.getParcelableExtra("video");
+    }
+
+    private void startPlayer() {
+        mVideoView = (VideoView) findViewById(R.id.video);
+        mVideoView.setVideoURI(uri);
+        mVideoView.setMediaController(mediaControls);
+        mediaControls.show();
+        mVideoView.start();
+        if ((position != null) && (position != 0))
+            mVideoView.seekTo(position);
+        mVideoView.setOnCompletionListener(this);
     }
 }
