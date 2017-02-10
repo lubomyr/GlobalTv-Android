a6f494a82096fd67303d31310b16e83540ef2b48
lubomyr <lubomyr31@gmail.com>
2017-02-10 20:04:57 +0200

    OpenUrl uses trim() function to remove space in start and end of sequence

--- app/src/main/java/atua/anddev/globaltv/service/ChannelServiceImpl.java
+++ app/src/main/java/atua/anddev/globaltv/service/ChannelServiceImpl.java
@@ -66,12 +66,8 @@ public class ChannelServiceImpl implements ChannelService {
         try {
             Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(chURL));
             browserIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            if (chURL.startsWith("http")) {
-                String url = chURL;
-                if (chURL.endsWith("\r"))
-                    url = chURL.substring(0, chURL.length() - 1);
-                browserIntent.setDataAndType(Uri.parse(url), "video/*");
-            }
+            if (chURL.startsWith("http"))
+                browserIntent.setDataAndType(Uri.parse(chURL.trim()), "video/*");
             context.startActivity(browserIntent);
         } catch (Exception e) {
             Log.i("GlobalTv", "Error: " + e.toString());
